677e26fffee37ee999cee4e21a9b13ec3008b2b9
greg <devim@outlook.it>
2018-10-09 21:47:18 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Description
+
+Small utility to print all the strings inside a binary file, compared to GNU strings **it supports also UTF-16 encoded strings**.
+
+## License
+
+GPLv3
---
+++ strings16.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    char curr, prev = 1;
+    bool printed = false;
+
+    if (argc < 2) {
+        cerr << "File path is missing" << endl;
+        return 1;
+    }
+
+    ifstream source(argv[1], ios_base::binary);
+    if (!source.good()) {
+        cerr << "Error while opening the file" << endl;
+        return 1;
+    }
+    while (source.get(curr)) {
+        if (isprint(curr))
+            cout << curr;
+        else if ((prev == 0) && curr)
+            cout << endl;
+        prev = curr;
+    }
+    source.close();
+
+    return 0;
+}
